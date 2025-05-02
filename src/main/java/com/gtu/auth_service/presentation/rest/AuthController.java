0dc2febfe79266866035da9d561508fed67aeb98
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-05-01 22:24:51 -0500

    Refactor AuthController to remove unused dependencies and streamline login functionality

--- src/main/java/com/gtu/auth_service/presentation/rest/AuthController.java
+++ src/main/java/com/gtu/auth_service/presentation/rest/AuthController.java
@@ -1,52 +1,25 @@
 package com.gtu.auth_service.presentation.rest;
 
-import java.util.HashMap;
-
+import com.gtu.auth_service.application.dto.LoginRequestDTO;
+import com.gtu.auth_service.application.dto.LoginResponseDTO;
+import com.gtu.auth_service.application.usecase.AuthUseCase;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.gtu.auth_service.infraestructure.client.PassengerClient;
-import com.gtu.auth_service.infraestructure.client.UserClient;
-import com.gtu.auth_service.infraestructure.service.JwtService;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
-@Tag(name = "Auth", description = "Endpoints for managing authentication")
-@CrossOrigin(origins = "*")
 public class AuthController {
-    private UserClient userClient;
-    private PassengerClient passengerClient;
-    private JwtService jwtService;
 
-    
+    private final AuthUseCase authUseCase;
 
-    public AuthController(UserClient userClient, PassengerClient passengerClient, JwtService jwtService) {
-        this.userClient = userClient;
-        this.passengerClient = passengerClient;
-        this.jwtService = jwtService;
+    public AuthController(AuthUseCase authUseCase) {
+        this.authUseCase = authUseCase;
     }
-    @GetMapping
-    @Operation(summary = "Test endpoint", description = "A test endpoint to check the service")
-    public ResponseEntity<HashMap<String, String>> test() {
-        final var user = userClient.getUserByEmail("admin@example.com");
-        final var passenger = passengerClient.getPassengerByEmail("test@example.com");
-        final var tokenUser = jwtService.generateToken(user);
-        final var tokenPassager = jwtService.generateToken(passenger);
-        final var response = new HashMap<String, String>();
-        response.put("user", user.getName());
-        response.put("passenger", passenger.getName());
-        response.put("tokenUser", tokenUser);
-        response.put("tokenPassenger", tokenPassager);
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = authUseCase.login(request);
         return ResponseEntity.ok(response);
     }
-
 }
-
