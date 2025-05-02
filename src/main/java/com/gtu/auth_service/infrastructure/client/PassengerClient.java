5004dffdc517f48cf93cc692d38111bb35d5763a
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-05-01 22:23:14 -0500

    Add PassengerClient and UserClient interfaces for user retrieval via Feign, and introduce UserServiceResponse DTO for user data representation

--- src/main/java/com/gtu/auth_service/infrastructure/client/PassengerClient.java
+++ src/main/java/com/gtu/auth_service/infrastructure/client/PassengerClient.java
@@ -1,11 +1,11 @@
 
-package com.gtu.auth_service.infraestructure.client;
+package com.gtu.auth_service.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gtu.auth_service.infraestructure.client.dto.UserServiceResponse;
+import com.gtu.auth_service.infrastructure.client.dto.UserServiceResponse;
 
 @FeignClient(name = "gtu-users-management-service", contextId = "passengerClient", path = "/internal/passengers")
 public interface PassengerClient {
--- src/main/java/com/gtu/auth_service/infrastructure/client/UserClient.java
+++ src/main/java/com/gtu/auth_service/infrastructure/client/UserClient.java
@@ -1,10 +1,10 @@
-package com.gtu.auth_service.infraestructure.client;
+package com.gtu.auth_service.infrastructure.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gtu.auth_service.infraestructure.client.dto.UserServiceResponse;
+import com.gtu.auth_service.infrastructure.client.dto.UserServiceResponse;
 
 @FeignClient(name = "gtu-users-management-service", contextId = "userClient", path = "/internal/users")
 public interface UserClient {
