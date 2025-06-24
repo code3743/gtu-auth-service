0007335dfc42fa9344a8afdbe4e3aafa8168cca4
SantiReRo19 <santiago.reyes.rodriguez@correounivalle.edu.co>
2025-06-24 15:39:12 -0500

    Delete LogCheckService and his use in AuthServiceApplication

--- src/main/java/com/gtu/auth_service/AuthServiceApplication.java
+++ src/main/java/com/gtu/auth_service/AuthServiceApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
-
-import com.gtu.auth_service.infrastructure.logs.LogCheckService;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,9 +13,4 @@ public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
-
-    @Bean
-	public CommandLineRunner checkInfrastructure(LogCheckService checkService) {
-		return args -> checkService.checkRabbitMQConnection();
-	}
 }
